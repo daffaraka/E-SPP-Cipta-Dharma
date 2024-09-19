949937904a0ced7562385786dfd43fd38b7d7b46
Daffa Raka <daffarakals@gmail.com>
2024-09-19 12:53:17 +0700

    First Commit

---
+++ database/migrations/2024_09_12_083505_create_biayas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('biayas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('nominal');
+            $table->string('nama_nominal');
+            $table->year('tahun');
+            $table->date('bulan');
+            $table->string('level');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('biayas');
+    }
+};
